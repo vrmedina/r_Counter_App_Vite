542e091fd59cca6110c6062521dbb10075b1e981
Victor Raul Medina Meza <vrmm1998@gmail.com>
2023-04-18 10:52:53 -0500

    hello world in react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <div>Hello World!</div>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    // StrictMode is a tool for highlighting potential problems in an application.
+    // Like Fragment, StrictMode does not render any visible UI.
+    // It activates additional checks and warnings for its descendants.
+    <StrictMode>
+        <App />
+    </StrictMode>
+);

248c16ad69a959a3df187c81d857d72606e106da
Victor Raul Medina Meza <vrmm1998@gmail.com>
2023-04-18 11:17:48 -0500

    create first component hello world

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,8 @@
+export const HelloWorldApp = () => {
+  return (
+    <div>Hello World App</div>
+  )
+}
+
+// Minimum react component
+//export const A=()=><>a</>
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-  return <div>Hello World!</div>;
-}
+import { HelloWorldApp } from './HelloWorldApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // StrictMode is a tool for highlighting potential problems in an application.
     // Like Fragment, StrictMode does not render any visible UI.
     // It activates additional checks and warnings for its descendants.
     <StrictMode>
-        <App />
+        <HelloWorldApp />
     </StrictMode>
 );

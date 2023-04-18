68019bbedae72eb9bc2669639e0cf73dcb4d7768
Victor Raul Medina Meza <vrmm1998@gmail.com>
2023-04-18 13:40:09 -0500

    add styles to the app

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,20 +1,9 @@
-// arrow function myname
-const myName = () => 'Victor!';
-
-const newMessage = {
-    title: 'Hello World',
-    body: 'This is my first app'
-}
-
 export const FirstApp = () => {
   return (
     // fragment
     <>
-        <h1>{myName()}</h1>
-        {/* <h1>{newMessage.title}</h1>
-        <p>{newMessage.body}</p>
-        <code>{JSON.stringify(newMessage)}</code> */}
-        {/* <p>My first app</p> */}
+        <h1>Victor</h1>
+        <p>My first React App</p>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
 
+// Import styles
+import './styles.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     // StrictMode is a tool for highlighting potential problems in an application.
     // Like Fragment, StrictMode does not render any visible UI.

05211c5c80134a7b1760ada3ecd8e3172f6040d2
Victor Raul Medina Meza <vrmm1998@gmail.com>
2023-04-21 12:11:02 -0500

    add test async without async await

--- src/base_pruebas/09-promesas.js
+++ src/base_pruebas/09-promesas.js
@@ -7,7 +7,7 @@ export const getHeroeByIdAsync = (id) => {
       if (p1) {
         resolve(p1);
       } else {
-        reject("No se pudo encontrar el héroe");
+        reject("No se pudo encontrar el héroe " + id);
       }
     }, 1000);
   });
--- test/base_pruebas/09-promesas.test.js
+++ test/base_pruebas/09-promesas.test.js
@@ -1,25 +1,30 @@
+import { getHeroeByIdAsync } from "../../src/base_pruebas/09-promesas";
+import heroes from "../../src/data/heroes";
+
 describe("Pruebas en 09-promesas", () => {
-    test("getHeroeByIdAsync debe retornar un heroe async", (done) => {
+    test("getHeroeByIdAsync debe retornar un heroe async", (done) => { // done is a callback function
         // 1. Arrange
         const id = 1;
 
         // 2. Act
-        getHeroeByIdAsync(id).then((heroe) => {
-        // 3. Assert
-            expect(heroe).toBe(heroes[0]);
-            done();
+        getHeroeByIdAsync(id)
+            .then((heroe) => {
+            // 3. Assert
+                expect(heroe).toBe(heroes[0]);
+                done(); 
         });
     });
 
-    test("getHeroeByIdAsync debe retornar un error si el heroe por id no existe", (done) => {
+    test("getHeroeByIdAsync debe retornar un error si el heroe por id no existe", () => {
         // 1. Arrange
         const id = 10;
-        
+    
         // 2. Act
-        getHeroeByIdAsync(id).catch((error) => {
+        getHeroeByIdAsync(id)
         // 3. Assert
-            expect(error).toBe("No se pudo encontrar el héroe");
-            done();
-        });
+            .catch((error) => {
+                expect(error).toBe(`No se pudo encontrar el héroe ${id}`);
+                done();
+            });
     });
 });

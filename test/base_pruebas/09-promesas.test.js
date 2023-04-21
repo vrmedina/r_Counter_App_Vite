67591c70e09be8a01bd842d1f6ca7b4c6196eacf
Victor Raul Medina Meza <vrmm1998@gmail.com>
2023-04-21 11:06:14 -0500

    add test async to promises

--- test/base_pruebas/09-promesas.test.js
+++ test/base_pruebas/09-promesas.test.js
@@ -1,22 +1,25 @@
 describe("Pruebas en 09-promesas", () => {
-  test("getHeroeByIdAsync debe retornar un heroe async", (done) => {
-    // 1. Arrange
-    const id = 1;
-    // 2. Act
-    getHeroeByIdAsync(id).then((heroe) => {
-      // 3. Assert
-      expect(heroe).toBe(heroes[0]);
-      done();
+    test("getHeroeByIdAsync debe retornar un heroe async", (done) => {
+        // 1. Arrange
+        const id = 1;
+
+        // 2. Act
+        getHeroeByIdAsync(id).then((heroe) => {
+        // 3. Assert
+            expect(heroe).toBe(heroes[0]);
+            done();
+        });
     });
-  });
-  test("getHeroeByIdAsync debe retornar un error si el heroe por id no existe", (done) => {
-    // 1. Arrange
-    const id = 10;
-    // 2. Act
-    getHeroeByIdAsync(id).catch((error) => {
-      // 3. Assert
-      expect(error).toBe("No se pudo encontrar el héroe");
-      done();
+
+    test("getHeroeByIdAsync debe retornar un error si el heroe por id no existe", (done) => {
+        // 1. Arrange
+        const id = 10;
+        
+        // 2. Act
+        getHeroeByIdAsync(id).catch((error) => {
+        // 3. Assert
+            expect(error).toBe("No se pudo encontrar el héroe");
+            done();
+        });
     });
-  });
 });

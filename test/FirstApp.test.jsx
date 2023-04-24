c0ad129689a835a68e579fbcc4591d90ede143dd
Victor Raul Medina Meza <vrmm1998@gmail.com>
2023-04-24 15:31:12 -0500

    add testing a component working

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -1,8 +1,9 @@
 const { render } = require("@testing-library/react")
-import FirstApp from '../src/FirstApp'
+import { FirstApp } from '../src/FirstApp'
 
 describe('Pruebas en <FirstApp />', () => {
     test('Debe coincidir con la captura', () => {
-        render(<FirstApp />)
+        const title = 'Hola soy Goku'
+        render(<FirstApp title={ title }/>)
     })
 })
